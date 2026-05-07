0821e53ea2415692cf9f7d9b2f8acabfb8e53071
tuhin <tuhin153695@gmail.com>
2026-05-07 12:23:49 +0600

    user register,login, logout functionality added

--- index.js
+++ index.js
@@ -10,12 +10,18 @@ const port = process.env.PORT || 5000;
 
 // Middlewares
 app.use(cors({
-  origin: ['http://localhost:3000'], // Adjust to your client URL
+  origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
   credentials: true
 }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan('dev'));
+
+// Request Logger
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`, req.body);
+  next();
+});
 
 // Database Connection and Server Start
 async function startServer() {
@@ -29,6 +35,13 @@ async function startServer() {
 
     // Routes
     app.use('/api/v1/classes', require('./src/routes/classRoutes'));
+    app.use('/api/v1/auth', require('./src/routes/authRoutes'));
+
+    // Error Handler
+    app.use((err, req, res, next) => {
+      console.error(err.stack);
+      res.status(500).json({ success: false, message: "Something went wrong on the server" });
+    });
 
     app.listen(port, () => {
       console.log(`Server is breathing on port ${port}`);
